0e4de827735794f3ddbfa1c2737283cde83df2b6
steven <stevenaguila@gmail.com>
2018-03-26 18:18:43 -0400

    Now caches db results

--- db/user.js
+++ db/user.js
@@ -7,6 +7,37 @@ const knex = require('knex')({
         database: 'niddtestdb'
     }
 });
+
+const records = [];
+
+knex('user')
+.then(results => {
+    for (user of results) {
+        records.push(user);
+    }
+
+    module.exports.findByUsername = (username, cb) => {
+        for (let i = 0, len = records.length; i < len; ++i) {
+            let record = records[i];
+            if (record.username === username) {
+                return cb(null, record);
+            }
+        }
+        return cb(null, null);
+    }
+
+    module.exports.findById = (id, cb) => {
+        const idx = id - 1;
+        if (records[idx]) {
+            cb(null, records[idx]);
+        }
+        else {
+            cb(`User ${id} not found`);
+        }
+    }
+});
+
+/*
 module.exports.findByUsername = (username, cb) => {
     return knex('user')
         .where('username', username)
@@ -40,3 +71,4 @@ module.exports.findById = (id, cb) => {
             console.log(err);
         });
 }
+*/
